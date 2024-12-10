605d5f3eda10205f06baba3b27a6b84b66a65678
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-12-11 03:00:17 +0700

    toggle Menu bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     {{-- Fontawsome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -19,5 +19,6 @@
        {{$slot}}
     </main>
 
+    <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>
