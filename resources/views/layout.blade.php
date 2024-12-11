9b47d501df77d6fdc7aa7a62213c1b379578aba8
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-12-11 12:54:29 +0700

    hero component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,12 +9,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>{{$title ?? 'Job Seeker'}}</title>
 </head>
 <body class="bg-gray-100">
 
     <x-header />
 
+    @if(request()->is('/'))
+    <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
        {{$slot}}
     </main>
